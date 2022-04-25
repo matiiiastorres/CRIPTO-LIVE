ba7f15eabe82c3b8059144cd8da478a065b2136e
MATIAS TORRES <matiiiastorres@gmail.com>
2022-04-25 03:09:22 -0300

    cambie color de la nav

--- JS/app.js
+++
@@ -1,5 +0,0 @@
-const getInfo = () => {
-    fetch("https://api.covalenthq.com/v1/1/address/demo.eth/balances_v2/?key=ckey_4bf5134147bb4813bca00d00975")
-    .then(resp => resp.json())
-    .then(json => console.log(json.results))
-}
